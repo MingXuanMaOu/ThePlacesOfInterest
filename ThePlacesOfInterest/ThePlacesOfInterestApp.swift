19299d9c5b5b224a9f70b831b930114595888937
MingXuMaOu <92174693+MingXuMaOu@users.noreply.github.com>
2023-01-08 09:58:09 +0800

    Initial Commit

---
+++ ThePlacesOfInterest/ThePlacesOfInterestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThePlacesOfInterestApp.swift
+//  ThePlacesOfInterest
+//
+//  Created by 刘铭 on 2023/1/8.
+//
+
+import SwiftUI
+
+@main
+struct ThePlacesOfInterestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
